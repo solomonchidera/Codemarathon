96b5c330728c9750e47764c2a7dfa3a1c5c8a534
SolomonChidera <solochidera10@gmail.com>
2023-07-05 17:28:16 +0100

    header file that I will be needing

---
+++ C-DIRECTORY/100D.h
@@ -0,0 +1,12 @@
+/* DEFINING MY HEADER FILE */
+#ifndef 100D_H
+#define 100D_H
+
+/* INCLUDING SOME C STANDARD HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+/* CLOSING MY HEADER FILE */
+#endif
