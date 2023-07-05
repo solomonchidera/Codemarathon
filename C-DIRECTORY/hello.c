5a7566bd36be1debf304944abfe1267b14a90c40
SolomonChidera <solochidera10@gmail.com>
2023-07-05 17:32:29 +0100

    A C file that prints my name and what am doing

---
+++ C-DIRECTORY/hello.c
@@ -0,0 +1,16 @@
+#include "100D.h"
+
+/**
+ * main - Ultimate function
+ * printf - C stdandard function that prints formatted data to the stdout
+ * Return - 0 if our program runs succesfully
+ */
+
+int main(void)
+{
+	char name[] = "SOLOMON CHIDERA";
+	printf("MY NAME IS %s AND AM STARTING MY 100-DAYS OF CODING NON-STOP");
+
+	return(0);
+}
+  
