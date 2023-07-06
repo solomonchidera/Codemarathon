4e92d7be683884456e80e982818768a47648d5ae
SolomonChidera <solochidera10@gmail.com>
2023-07-06 18:03:48 +0100

    Creating a number guessing game that computer generates a random number and ask you to guess

---
+++ PYTHON-DIRECTORY/Num_Guess.py
@@ -0,0 +1,34 @@
+import random
+
+def Num_Guess():
+	generated_number = random.randint(1, 10)
+	attempts = 3
+    
+	while attempts > 0:
+	name = input("please what is your name")
+		try:
+            user_guess = int(input("Guess the number (between 1 and 10): "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+        
+        if user_guess < generated_number:
+            print("Less than the generated number.")
+        elif user_guess > generated_number:
+            print("Greater than the generated number.")
+        else:
+            print("Congratulations! You guessed the number correctly.")
+            print(f"The generated number was: {generated_number}")
+            break
+        
+        attempts -= 1
+        print(f"Attempts left: {attempts}")
+    
+    else:
+        print(f"Game over! The number was {generated_number}.")
+    
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == "yes":
+        play_game()
+
+Num_Guess()
