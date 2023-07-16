159a7103475da665afe03e6edeea8cc486c3ff8e
SolomonChidera <solochidera10@gmail.com>
2023-07-16 22:34:51 +0100

    working with array

---
+++ C-DIRECTORY/array_string/array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char students[][50] = {"amarachi", "rejoice", "kelvin", "peace", "elijah", "solomom"};
+
+		puts( students[5]);
+
+	return(0);
+}
