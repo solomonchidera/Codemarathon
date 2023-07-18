15e74fcdb8c6829b2137571cc03447bb8e1c2e86
SolomonChidera <solochidera10@gmail.com>
2023-07-19 00:08:06 +0100

    A program that prints the alphabet but without 'e' and 'q'

---
+++ C-DIRECTORY/varloops/dont_print_q_e.c
@@ -0,0 +1,16 @@
+#include "chi.h"
+
+int main(void)
+{
+	char start = 'a';
+	char stop = 'z';
+
+	for (; start <= stop; start++)
+	{
+		if (start != 'e' && 'q')
+			putchar(start);
+	}
+	putchar(10);
+
+	return(0);
+}
