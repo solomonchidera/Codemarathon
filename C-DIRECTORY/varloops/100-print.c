5b9476657aeea7fe695439756eff83da6b7b3ef4
SolomonChidera <solochidera10@gmail.com>
2023-07-20 12:16:29 +0100

    A program that prints all possible different combinations of two digits.

--- C-DIRECTORY/varloops/100-print.c
+++ C-DIRECTORY/varloops/100-print.c
@@ -3,14 +3,25 @@
 int main(void)
 {
 	int i = 0;
-	for (; i < 90; i++)
+	while (i < 100)
 	{
-		putchar(i + 48);
-		if (i < 10)
+		int a = i / 10;
+		int b = i % 10;
 
-			putchar(0 + i);
-		else
-			putchar(i);
+		if (a < b)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+
+			if (i != 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		i++;
+		
 	}
 	putchar(10);
 	return(0);
