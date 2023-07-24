23820b40881ddde2bac41f1fafca32c015692b10
SolomonChidera <solochidera10@gmail.com>
2023-07-24 17:04:50 +0100

    a function that print the sign of a number

---
+++ C-DIRECTORY/Function-And-Nested-Loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "chi.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return(1);
+
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return(0);
+
+	} else if (n < 0)
+	{
+		_putchar(45);
+		return(-1);
+	}
+
+	return(0);
+}
