ecd4cee92c8198fbb15d7194f6374c3aa3911784
SolomonChidera <solochidera10@gmail.com>
2023-07-25 15:16:58 +0100

    printing the size of a pointer

---
+++ C-DIRECTORY/POINTER/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - printing the size, in bytes of a pointer
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int *ptr;
+
+	printf("Size of pointer: %lu\n", sizeof(ptr));
+	return(0);
+}
