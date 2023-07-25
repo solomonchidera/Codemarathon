e86129328ef3cbbcb178b326618fad90045bb527
SolomonChidera <solochidera10@gmail.com>
2023-07-25 13:05:43 +0100

    printing the value of a variable with pointer using thr valueof operator or the dereferencing operator

---
+++ C-DIRECTORY/POINTER/char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+	/*Initializing a variable named x*/
+	int x = 5;
+	/*initializing a pointer*/
+	int *ptr;
+	ptr = &x;
+	printf("%d\n", *ptr);
+
+	return(0);
+}
