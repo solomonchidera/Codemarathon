9a6ef144f02550b0b544b7602de34ca1007f6f1d
SolomonChidera <solochidera10@gmail.com>
2023-07-25 15:23:49 +0100

    printing the address of a pointer

---
+++ C-DIRECTORY/POINTER/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing the address of a pointer
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int *ptr;
+	
+	printf("Address of pointer 'ptr': %p\n", &ptr);
+	return(0);
+}
