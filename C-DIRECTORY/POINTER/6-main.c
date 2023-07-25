2932812f9bc5d6ff428cffe4d2a5dfb745c8e002
SolomonChidera <solochidera10@gmail.com>
2023-07-25 19:50:36 +0100

    derefencing pointers, example with int and char types

---
+++ C-DIRECTORY/POINTER/6-main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main(void)
+{
+	int n;
+	int *p;
+	char c;
+	char *pp;
+
+	n = 40;
+	p = &n;
+	c = 'A';
+	pp = &c;
+
+
+	printf("Value of 'c': %d ('%c'\n)", c, c);
+	printf("Address of 'c':  %p\n", &c);
+	printf("Value of f 'pp': %p\n", pp);
+	printf("Value of 'n': %d\n", n);
+	printf("Address of n: %p\n", &n);
+	printf("Value of p: %p\n", p);
+	*p = 402;
+	*pp = 'o';
+	printf("Value of 'n': %d\n", n);
+	printf("Value of '*pp': %d\n", *pp);
+	printf("Value of 'c': %d ('%c')\n", c, c);
+	printf("Value of '*pp': %d ('%c')\n", *pp, *pp);
+	return (0);
+}
