49ea239a17af897abd06094f86d7f42d158f36f4
SolomonChidera <solochidera10@gmail.com>
2023-07-25 23:54:45 +0100

    things you can't do with an array

---
+++ C-DIRECTORY/POINTER/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - You can not modify a 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[5];
+	int b;
+	int c[5];
+
+	a = 0; /* nop */
+	a = &b; /* nop */
+	a = c; /* nop */
+	return (0);
+}
