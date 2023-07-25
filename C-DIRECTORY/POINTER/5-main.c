7755bf0805fffea98266f0710f8c6e18f6f55732
SolomonChidera <solochidera10@gmail.com>
2023-07-25 17:29:28 +0100

    derefencing with a pointer to manipulate the value of a variable

---
+++ C-DIRECTORY/POINTER/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+	printf("Value of 'n': %d\n", n);
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+	*p = 402;
+	printf("Value of 'n': %d\n", n);
+
+	return(0);
+}
