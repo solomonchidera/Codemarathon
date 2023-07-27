6b5f12f6efbd8136349daaa3fec7af8153bb225f
Hanane-96 <hananelassale1996@gmail.com>
2023-07-27 08:50:08 +0100

    Function that still checks for lastdigit (was trying to remember what i have been doing)

---
+++ C-DIRECTORY/Function-And-Nested-Loops/nlast.c
@@ -0,0 +1,19 @@
+#include "chi.h"
+#include "main.h"
+
+int print_last_digit(int z)
+{
+	int a;
+	int me = z % 10;
+	if (z < 0)
+	{
+		_putchar(-z + 48);
+		return(me)
+
+	}
+	else
+	{
+		_puchar(z + 48);
+		return(me)
+	}
+}
