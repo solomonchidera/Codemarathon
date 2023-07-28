7fce116e2534041e6617ce456be247f477001ceb
SolomonChidera <solochidera10@gmail.com>
2023-07-28 16:52:42 +0100

    Double function printing the area of a triangle

--- C-DIRECTORY/Function-And-Nested-Loops/callarea.c
+++ C-DIRECTORY/Function-And-Nested-Loops/callarea.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+int areaof(int len, int width)
+{
+	int c = len * width;
+	return(c);
+}
 int main(void)
 {
 	int a = 30, b = 10;
 	int rec_area;
-	rec_area = area(a, b);
+	rec_area = areaof(a, b);
 	printf("%d\n", rec_area);
 	return(0);
 }
