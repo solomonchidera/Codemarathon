7ae11861ba146066e962dc5f2c8a3ac1b30a3a35
SolomonChidera <solochidera10@gmail.com>
2023-07-31 07:33:55 +0100

    Function that prints time from 00:00 to 23:59

--- C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
+++ C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 		{
 				_putchar((hour / 10) + '0');
 				_putchar((hour % 10) + '0');
-				_putchar(hour + ':');
+				_putchar(':');
 				_putchar((minutes / 10) + '0');
 				_putchar((minutes % 10) + '0');
 				_putchar(10);
