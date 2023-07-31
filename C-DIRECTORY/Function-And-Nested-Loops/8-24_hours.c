f43284b19e21fbb0d997bcc4cb06b66ebb913e36
SolomonChidera <solochidera10@gmail.com>
2023-07-31 07:46:33 +0100

    Function that prints time from 00:00 to 23:59

--- C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
+++ C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
@@ -3,9 +3,9 @@
 void jack_bauer(void)
 {
 	int hour,minutes = 0;
-	for (; hour <= 24; hour++)
+	for (; hour < 24; hour++)
 	{
-		for (; minutes <= 60; minutes++)
+		for (; minutes < 60; minutes++)
 		{
 			printf("%02d:%02d", hour, minutes);
 		}
