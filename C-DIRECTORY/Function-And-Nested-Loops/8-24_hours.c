69d74698c727f130973a79d4e69c8d3a967f6538
SolomonChidera <solochidera10@gmail.com>
2023-07-31 08:10:51 +0100

    printing correct now just needed to add a newline

--- C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
+++ C-DIRECTORY/Function-And-Nested-Loops/8-24_hours.c
@@ -2,12 +2,18 @@
 
 void jack_bauer(void)
 {
-	int hour,minutes = 0;
+	int hour = 0,minutes;
 	for (; hour < 24; hour++)
 	{
+		minutes = 0;
+
 		for (; minutes < 60; minutes++)
 		{
-			printf("%02d:%02d", hour, minutes);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 		}
 	}
 }
