ea296ec41cc5882c02c99c1119ec2a7ce0da3864
SolomonChidera <solochidera10@gmail.com>
2023-08-07 23:50:29 +0100

    We learn everyday

---
+++ C-DIRECTORY/long.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+	int64_t bigger_int = 107383629290927277;
+	printf("I think it will print, the value is %d\n", bigger_int);
+	return(0);
+}
