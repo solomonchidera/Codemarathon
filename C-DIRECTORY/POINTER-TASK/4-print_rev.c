9771e3d36a9114d0372462b5caac9da4a556a393
SolomonChidera <solochidera10@gmail.com>
2023-08-27 16:33:54 +0100

    Function that prints a string in reverse

---
+++ C-DIRECTORY/POINTER-TASK/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*s);
+	}
+	_putchar(10);
+}
