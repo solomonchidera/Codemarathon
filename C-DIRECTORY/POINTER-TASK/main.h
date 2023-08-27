5a38c14d24309b8f67779e11d12db855624ba920
SolomonChidera <solochidera10@gmail.com>
2023-08-27 15:39:41 +0100

    header file with prototypes

---
+++ C-DIRECTORY/POINTER-TASK/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void print_rev(char *s);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+nt _atoi(char *s);
+
+#endif
